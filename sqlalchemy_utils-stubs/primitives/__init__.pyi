b1bb8260ad01c0540af7a31f8c90f03a371936a5
Stefan Mototolea <stefan.mototolea@binovate.com>
2024-05-13 10:30:52 +0300

    chore(deps): update dependencies (#71)

--- sqlalchemy_utils-stubs/primitives/__init__.pyi
+++ sqlalchemy_utils-stubs/primitives/__init__.pyi
@@ -3,4 +3,3 @@ from .currency import Currency as Currency
 from .ltree import Ltree as Ltree
 from .weekday import WeekDay as WeekDay
 from .weekdays import WeekDays as WeekDays
-
--- sqlalchemy_utils-stubs/primitives/weekdays.pyi
+++ sqlalchemy_utils-stubs/primitives/weekdays.pyi
@@ -1,16 +1,11 @@
-from __future__ import annotations
-
-from typing import Hashable, Generator, Union
+from collections.abc import Hashable
+from collections.abc import Iterator
 
 from ..utils import str_coercible as str_coercible
 from .weekday import WeekDay as WeekDay
 
 @str_coercible
 class WeekDays:
-    def __init__(self, bit_string_or_week_days: Union[str, WeekDays, Hashable]) -> None:
-        pass
-    def __iter__(self) -> Generator[WeekDay, None, None]:
-        pass
-    def as_bit_string(self) -> str:
-        pass
-
+    def __init__(self, bit_string_or_week_days: str | WeekDays | Hashable) -> None: ...
+    def __iter__(self) -> Iterator[WeekDay]: ...
+    def as_bit_string(self) -> str: ...
