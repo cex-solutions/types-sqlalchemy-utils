ea7476f6559d9d52251d292f95b926f22b77bf1d
Eitan Mosenkis <eitan@mosenkis.net>
2024-05-13 11:22:41 +0300

    feat: add annotations for types.phone_number. (#70)

--- sqlalchemy_utils-stubs/types/phone_number.pyi
+++ sqlalchemy_utils-stubs/types/phone_number.pyi
@@ -1,6 +1,24 @@
+import phonenumbers as phonenumbers
+from phonenumbers.phonenumber import PhoneNumber as BasePhoneNumber
+from phonenumbers.phonenumberutil import NumberParseException
+from sqlalchemy import exc
+from sqlalchemy import types
+
 from ..exceptions import ImproperlyConfigured as ImproperlyConfigured
 from ..utils import str_coercible as str_coercible
-from .scalar_coercible import ScalarCoercible as ScalarCoercible
-from typing import Any
 
-def __getattr__(name: str) -> Any: ...  # incomplete
+class PhoneNumberParseException(NumberParseException, exc.DontWrapMixin): ...
+
+class PhoneNumber(BasePhoneNumber):
+    region: str
+    national: str
+    international: str
+    e164: str
+    def __init__(self, raw_number: str, region: str | None = None, check_region: bool = True) -> None: ...
+    def is_valid_number(self) -> bool: ...
+
+class PhoneNumberType(types.TypeDecorator[PhoneNumber]):
+    STORE_FORMAT: str
+    impl: types.Unicode
+    python_type: type[PhoneNumber]
+    def __init__(self, region: str = "US", max_length: int = 20, *args, **kwargs) -> None: ...
